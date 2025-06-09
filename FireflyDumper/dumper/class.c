e79fc0231d146ef0a742c2a049321d14c1af26bb
🐱 <119447842+gatokeeper@users.noreply.github.com>
2025-06-09 03:36:09 +0200

    fix: field flags

--- FireflyDumper/dumper/class.c
+++ FireflyDumper/dumper/class.c
@@ -4,23 +4,41 @@
 void get_class_modifiers(Il2CppClass* class, FILE* f) {
     uint32_t flags = Il2CppFunctions_t.class_get_flags(class);
 
-    if (flags & TYPE_ATTRIBUTE_PUBLIC || flags & TYPE_ATTRIBUTE_NESTED_PUBLIC) {
-        fprintf(f, "public ");
-    } else if (flags & TYPE_ATTRIBUTE_NESTED_FAM_OR_ASSEM) {
-        fprintf(f, "protected internal ");
-    } else if (flags & TYPE_ATTRIBUTE_NESTED_FAMILY) {
-        fprintf(f, "protected ");
-    } else if (flags & TYPE_ATTRIBUTE_NESTED_ASSEMBLY) {
-        fprintf(f, "internal ");
-    } else if (flags & TYPE_ATTRIBUTE_NESTED_FAM_AND_ASSEM) {
-        fprintf(f, "private protected ");
-    } else if (flags & TYPE_ATTRIBUTE_NESTED_PRIVATE) {
-        fprintf(f, "private ");
+    uint32_t visibility = flags & TYPE_ATTRIBUTE_VISIBILITY_MASK;
+    switch (visibility) {
+        case TYPE_ATTRIBUTE_PUBLIC:
+        case TYPE_ATTRIBUTE_NESTED_PUBLIC:
+            fprintf(f, "public ");
+            break;
+        case TYPE_ATTRIBUTE_NOT_PUBLIC:
+        case TYPE_ATTRIBUTE_NESTED_ASSEMBLY:
+            fprintf(f, "internal ");
+            break;
+        case TYPE_ATTRIBUTE_NESTED_PRIVATE:
+            fprintf(f, "private ");
+            break;
+        case TYPE_ATTRIBUTE_NESTED_FAMILY:
+            fprintf(f, "protected ");
+            break;
+        case TYPE_ATTRIBUTE_NESTED_FAM_AND_ASSEM:
+            fprintf(f, "private protected ");
+            break;
+        case TYPE_ATTRIBUTE_NESTED_FAM_OR_ASSEM:
+            fprintf(f, "protected internal ");
+            break;
+        default:
+            fprintf(f, "/* unknown visiblity type */"); // should never happen
+            break;
     }
-
-    // Others
-    if (flags & TYPE_ATTRIBUTE_ABSTRACT) {
+    
+    if ((flags & TYPE_ATTRIBUTE_ABSTRACT) && (flags & TYPE_ATTRIBUTE_SEALED)) {
+        fprintf(f, "static ");
+    } else if (!(flags & TYPE_ATTRIBUTE_INTERFACE) && (flags & TYPE_ATTRIBUTE_ABSTRACT)) {
         fprintf(f, "abstract ");
+    } else if (!Il2CppFunctions_t.class_is_valuetype(class) &&
+               !Il2CppFunctions_t.class_is_enum(class) &&
+               (flags & TYPE_ATTRIBUTE_SEALED)) {
+        fprintf(f, "sealed ");
     }
 
     if (flags & TYPE_ATTRIBUTE_INTERFACE) {
